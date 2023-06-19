9942be1882e625d9f77f215aef3a39b3cc3c5c77
Hasan Emir TUKOZOGLU <tukozogluster@gmail>
2023-06-19 12:16:13 +0200

    solution 2

--- myProject/myProject/src/main/java/com/wildcodeschool/myProject/controller/DoctorController.java
+++ myProject/myProject/src/main/java/com/wildcodeschool/myProject/controller/DoctorController.java
@@ -1,28 +1,26 @@
 package com.wildcodeschool.myProject.controller;
 
+import com.wildcodeschool.myProject.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/1")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public String getDoctorOne() {
-        return "William Hartnell";
+    public Doctor getNumber(@PathVariable int number) {
+        if (number == 13) {
+            Doctor doctor = new Doctor(13, "Jodie Whittaker");
+            return doctor;
+        } else if (number >= 1 && number <= 12) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation {number}");
+        }
+        }
     }
-
-    @GetMapping("/doctor/10")
-    @ResponseBody
-    public String getDoctorTen() {
-        return "David Tennant";
-    }
-
-    @GetMapping("/doctor/13")
-    @ResponseBody
-    public String getDoctorThirteen() {
-        return "Jodie Whittaker";
-    }
-
-}
